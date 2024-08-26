ac307fefed4c67a71b8af6315e31aa67e079dd52
ClaraMacena <clara17_macena@hotmail.com>
2024-08-26 19:07:37 -0300

    Criação Cliente

--- src/main/java/com/distribuidora/distribuidora/controller/ClienteController.java
+++ src/main/java/com/distribuidora/distribuidora/controller/ClienteController.java
@@ -1,5 +1,23 @@
 package com.distribuidora.distribuidora.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.distribuidora.distribuidora.service.ClienteService;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3608)
+@RequestMapping("/cliente")
+
 public class ClienteController {
+    
+    final ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    
     
 }
--- src/main/java/com/distribuidora/distribuidora/service/ClienteService.java
+++ src/main/java/com/distribuidora/distribuidora/service/ClienteService.java
@@ -1,5 +1,15 @@
 package com.distribuidora.distribuidora.service;
 
+import org.springframework.stereotype.Service;
+import com.distribuidora.distribuidora.repository.ClienteRepository;
+
+@Service //ponto de injeção de dependências
 public class ClienteService {
     
+    final ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
 }
